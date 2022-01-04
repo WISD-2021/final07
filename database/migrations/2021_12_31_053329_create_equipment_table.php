4a896d1c40786ffb20fb865d6c5f69d526635d75
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2022-01-04 23:36:15 +0800

    建立equipments的欄位

--- database/migrations/2021_12_31_053329_create_equipment_table.php
+++ database/migrations/2021_12_31_053329_create_equipment_table.php
@@ -15,6 +15,18 @@ class CreateEquipmentTable extends Migration
     {
         Schema::create('equipment', function (Blueprint $table) {
             $table->id();
+            $table->string('name',20)->comment('器材名稱');
+            $table->string('eqinformation')->comment('器材資訊');
+            $table->integer('allcount')->unsigned()->comment('總數量');
+            $table->integer('rentcount')->unsigned()->comment('目前租出數量');
+            $table->integer('inventory')->unsigned()->comment('庫存數量');
+            $table->integer('price')->unsigned()->comment('器材單價');
+            $table->integer('rentprice')->unsigned()->comment('租借價格(單項兩天一夜價格)');
+            $table->integer('claimprice')->unsigned()->comment('賠償單價');
+            $table->string('img')->comment('圖片');
+            $table->integer('cleanfee')->unsigned()->comment('清潔費');
+            $table->date('maintain')->nullable()->comment('保養日期');
+            $table->foreignId('manager_id')->nullable()->index()->comment('保養者');
             $table->timestamps();
         });
     }
