d94f95865dfd905cbb0e7de50c245e5870ca3540
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2022-01-05 00:27:27 +0800

    修正orders欄位和新增一個send_date欄位

--- database/migrations/2021_12_31_052927_create_orders_table.php
+++ database/migrations/2021_12_31_052927_create_orders_table.php
@@ -15,7 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('member_id')->nullable()->index();
+            $table->foreignId('member_id')->index();
             $table->integer('total')->unsigned()->comment("總額");
             $table->string('status',20)->comment('租賃狀態');
             $table->date('rent_date')->comment('租用日');
@@ -23,7 +23,8 @@ class CreateOrdersTable extends Migration
             $table->dateTime('pickup_date')->comment('取貨日');
             $table->integer('clean_fee')->nullable()->unsigned()->comment('清潔費');
             $table->integer('damages')->nullable()->unsigned()->comment('賠償價');
-            $table->foreignId('manager_id')->nullable()->index()->comment('交接者');
+            $table->dateTime('send_date')->comment('送出日期');
+            $table->foreignId('manager_id')->index()->comment('交接者');
             $table->timestamps();
         });
     }
