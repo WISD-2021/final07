5002aa829521b0a918f57ef19a8c7f23ca153440
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2022-01-04 23:19:59 +0800

    建立orders的欄位

--- database/migrations/2021_12_31_052927_create_orders_table.php
+++ database/migrations/2021_12_31_052927_create_orders_table.php
@@ -15,6 +15,15 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('member_id')->nullable()->index();
+            $table->integer('total')->unsigned()->comment("總額");
+            $table->string('status',20)->comment('租賃狀態');
+            $table->date('rent_date')->comment('租用日');
+            $table->date('return_date')->comment('歸還日');
+            $table->dateTime('pickup_date')->comment('取貨日');
+            $table->integer('clean_fee')->nullable()->unsigned()->comment('清潔費');
+            $table->integer('damages')->nullable()->unsigned()->comment('賠償價');
+            $table->foreignId('manager_id')->nullable()->index()->comment('交接者');
             $table->timestamps();
         });
     }
