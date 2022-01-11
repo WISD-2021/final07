23fc5e707686d32efe83da49c6eed676b110038b
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2022-01-11 22:14:12 +0800

    建立會員端的Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Member;
+use App\Models\User;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CartItemController;
+use App\Http\Controllers\OrderDetailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +21,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('index', function () {
+    return view('home');
+});
+
+//會員
+//個人資料
+Route::middleware(['auth:sanctum', 'verified'])->get('/member/{member}',[MemberController::class,'index'])->name('member.index');//顯示個人資料
+Route::middleware(['auth:sanctum', 'verified'])->get('/member/{member}/edit',[MemberController::class,'edit'])->name('member.edit');//編輯個人資料
+Route::middleware(['auth:sanctum', 'verified'])->put('/member/{member}',[MemberController::class,'update'])->name('member.update');//更新個人資料
+
+//器材
+Route::middleware(['auth:sanctum', 'verified'])->get('/rentequipment',[EquipmentController::class,'index'])->name('rentequipment.index');//顯示器材
+Route::middleware(['auth:sanctum', 'verified'])->get('/rentequipment/{equipment}',[EquipmentController::class,'show'])->name('rentequipment.show');//顯示詳細器材資訊
+
+//購物車
+Route::middleware(['auth:sanctum', 'verified'])->get('/rentcart',[CartItemController::class,'index'])->name('rentcart.index');//顯示購物車內容
+Route::middleware(['auth:sanctum', 'verified'])->delete('/rentcart/{rentcart}',[CartItemController::class,'destroy'])->name('rentcart.destroy');//刪除購物車項目
+Route::middleware(['auth:sanctum', 'verified'])->post('/rentcart',[CartItemController::class,'store'])->name('rentcart.add');//購物車送出
+
+//租賃單
+Route::middleware(['auth:sanctum', 'verified'])->get('/renteorder',[OrderController::class,'index'])->name('renteorder.index');//顯示租賃單
+Route::middleware(['auth:sanctum', 'verified'])->post('/renteorder',[OrderController::class,'store'])->name('renteorder.store');//儲存租賃單
+Route::middleware(['auth:sanctum', 'verified'])->get('/renteorder/{rentorder}/items',[OrderController::class,'show'])->name('renteorder.items.show');//搜勳的租賃單
+//租賃單明細
+Route::middleware(['auth:sanctum', 'verified'])->get('/renteorder/{rentorder}/detail',[OrderDetailController::class,'show'])->name('rodetail.show');//顯示租賃單明細
+
