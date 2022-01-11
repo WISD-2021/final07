cc1cb4b5404029423c5bc0994e01bc2b1cb6445f
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2022-01-11 17:00:39 +0800

    前端共同的模板

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,85 @@
+<?php
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\CartItemController;
+use App\Http\Controllers\OrderController;
+use App\Models\Cart_item;
+?>
+<!--::header part start::-->
+    <header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="{{ url('/')}}"> <img src="img/logo-2.png" alt="登山露營器材租借" width="200px"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="menu_icon"><i class="fas fa-bars"></i></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+                            <ul class="navbar-nav">
+                                <!--<li class="nav-item">
+                                    <a class="nav-link" href="index.html">Home</a>
+                                </li>-->
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('about')}}">關於我們</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('rentequipment')}}">裝備租賃</a>
+                                </li>
+
+
+                                <div class="hearer_icon d-flex align-items-center">
+                                    <a id="search_1" href="javascript:void(0)"><i class="ti-search"></i></a>
+                                    <a href="cartitem/cart.blade.php">
+                                        <i class="flaticon-shopping-cart-black-shape"></i>
+                                    </a>
+                                    @if (Route::has('login'))
+                                            @auth
+                                                <a href="{{ url('/')}}" class="text-sm text-gray-700 dark:text-gray-500 underline"></a>
+                                                <li class="nav-item dropdown">
+                                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                        11
+                                                    </a>
+                                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                                                        <a class="dropdown-item" href="{{ url('memeber')}}"> 個人資料</a><!--還沒設計好-->
+                                                        <a class="dropdown-item" href="{{ url('Order')}}">租賃單</a><!--還沒設計好-->
+                                                    </div>
+                                                </li>
+                                                <!--登出-->
+                                                    <li>
+                                                        <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                                            <i class="glyphicon glyphicon-log-out"></i>
+                                                        </a>
+                                                    </li>
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                        @csrf
+                                                    </form>
+                                                <!--登出-->
+                                            @else
+                                                <a href="{{ route('login') }}" ><i class="glyphicon glyphicon-user"></i></a>
+
+                                                @if (Route::has('register'))
+                                                    <a href="{{ route('register') }}" ><i class="fas fa-registered"></i></a>
+                                                @endif
+                                            @endauth
+                                    @endif
+                                </div>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="search_input" id="search_input_box">
+            <div class="container ">
+                <form class="d-flex justify-content-between search-inner">
+                    <input type="text" class="form-control" id="search_input" placeholder="Search Here">
+                    <button type="submit" class="btn"></button>
+                    <span class="ti-close" id="close_search" title="Close Search"></span>
+                </form>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title')</title>
+    <link rel="icon" href="img/camping.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="css/all.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+
+@include('layouts.header')
+
+@yield('content')
+<!--::footer-->
+@include('layouts.footer')
+
+
+    <!-- jquery plugins here-->
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <!-- popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- magnific popup js -->
+    <script src="js/jquery.magnific-popup.js"></script>
+    <!-- carousel js -->
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <!-- slick js -->
+    <script src="js/slick.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <!-- custom js -->
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
