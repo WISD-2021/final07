e77f70543cdfe8372ee315ad7d023cba334c4697
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2022-01-16 14:11:59 +0800

    註冊資料庫匯入資料庫

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -2,11 +2,13 @@
 
 namespace App\Actions\Fortify;
 
+use App\Models\Member;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
+use Illuminate\Support\Facades\DB;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -16,7 +18,7 @@ class CreateNewUser implements CreatesNewUsers
      * Validate and create a newly registered user.
      *
      * @param  array  $input
-     * @return \App\Models\User
+     * @return User|int|string
      */
     public function create(array $input)
     {
@@ -25,14 +27,32 @@ class CreateNewUser implements CreatesNewUsers
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'type' => ['required','boolean'],
+            'identity'=>['required','string','max:20','unique:members'],
+            'phone'=>['required','string','max:20'],
+            'address'=>['required','string','max:30'],
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
         ])->validate();
 
-        return User::create([
+        $usc=User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
             'type'=>$input['type'],
         ]);
+        $usnum=DB::table('users')->count();
+        if($usnum<=0){
+            $idmax=0;
+        }else{
+            $usnumid=DB::table('users')->max('id');
+            $idmax=$usnumid;
+        }
+        //$idmax=$idmax+1;
+        Member::create([
+            'user_id'=>$idmax,
+            'identity' => $input['identity'],
+            'phone' => $input['identity'],
+            'address' => $input['address'],
+        ]);
+        return $usc;
     }
 }
